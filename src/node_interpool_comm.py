ee205a48b5ef4c29a2010691b75c9772ad1b8f5e
HendrikLeier <hendrik.leier75@gmail.com>
2021-03-15 10:08:36 -0700

    Add interpool communication logic

---
+++ src/node_interpool_comm.py
@@ -0,0 +1,78 @@
+import uuid
+import hashlib
+import random
+import string
+
+def hash_two_uuids(uuid1, uuid2):
+    m = hashlib.sha256()
+    m.update(uuid1.bytes)
+    m.update(uuid2.bytes)
+    return m.digest()
+
+
+# input params are lists of uuid's
+def get_lists_for_nodes(my_uuid, my_members, target_pool, nodes_in_pool):
+    source_pool = [my_uuid] + my_members
+    target_pool.sort()
+
+    hashes_for_targets = {t: [] for t in target_pool}
+
+    for t in target_pool:
+        for s in source_pool:
+            the_hash = hash_two_uuids(t, s)
+            hashes_for_targets[t].append((s, the_hash))
+
+    return source_pool, target_pool, hashes_for_targets
+
+
+def get_closest_node(message, target_nodes, hashes_for_targets):
+    msg_hash = hashlib.sha256(message.encode()).digest()
+    results = []
+    
+    for t in target_nodes:
+        min_ = int(2**256) + 1
+        node = -1
+        
+        result_stack = [(node, min_)]
+        
+        for source_uuid, hsh in hashes_for_targets[t]:
+            val = int.from_bytes(msg_hash, "big") ^ int.from_bytes(hsh, "big")
+            result_stack.append((source_uuid, val))
+        
+        # sort by val
+        result_stack.sort(key=lambda x: x[1])
+        results.append([(a[0], t) for a in result_stack])
+        
+    return results
+
+
+def pick_two_equal_distrib(array, target_nodes, source_nodes):
+    results = {t: [] for t in target_nodes}
+    times_parent_used = {s: 0 for s in source_nodes}
+    
+    for child_num, child_id in enumerate(target_nodes):
+        num_inserts = 0
+        for tpl in array[child_num]:
+            if num_inserts == 2:
+                break
+            if times_parent_used[tpl[0]] < 2:
+                if child_num == len(target_nodes) - 2 and num_inserts == 1:
+                    zero_entry = [item for item in times_parent_used.items() if item[1]==0]
+                    if len(zero_entry) > 0:
+                        zero_entry = zero_entry[0]
+                        underused_tpl = [a for a in array[child_num] if a[0] == zero_entry[0]][0]
+                        results[child_id].append(underused_tpl)
+                        times_parent_used[underused_tpl[0]] += 1
+                        num_inserts += 1
+                        continue
+                
+                results[child_id].append(tpl)
+                times_parent_used[tpl[0]] += 1
+                num_inserts += 1
+    
+    return results
+
+
+def calculate_allocs(message, target_nodes, source_nodes, hashes_for_targets):
+    array = get_closest_node(message, target_nodes, hashes_for_targets)
+    return pick_two_equal_distrib(array, target_nodes, source_nodes)
---
+++ src/tests/node_interpool_comm_test.py
@@ -0,0 +1,71 @@
+import unittest
+import string
+import random
+import node_interpool_comm as nic
+import uuid
+
+def res_list_to_str(list_):
+    result = ""
+    for a in list_:
+        if type(a) == list or type(a) == tuple:
+            result += res_list_to_str(a)
+        else:
+            result += str(a)
+    return result
+
+
+def random_string(n):
+    return ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(n))
+
+
+class TestNodeInterpoolComm(unittest.TestCase):
+    def setUp(self):
+        self.msg = "dfsdouigfsdiufgsdf"
+        self.targets = [uuid.uuid4() for _ in range(5)]
+        self.src = [uuid.uuid4() for _ in range(5)]
+        
+        self.input_sets = []
+        for i in range(len(self.src)):
+            me = self.src[i]
+            others = self.src[:i]+self.src[i+1:]
+            self.input_sets.append((me, others))
+
+    def test_hash_equality(self):
+        res_str_pre = ""
+
+        for i in range(5):
+            me = self.input_sets[i][0]
+            others = self.input_sets[i][1]
+
+            src_pool, trgt_pool, hsh_fr_trgt = nic.get_lists_for_nodes(me, others, self.targets, len(self.targets))
+
+            array = nic.get_closest_node(self.msg, trgt_pool, hsh_fr_trgt)
+
+            results = nic.pick_two_equal_distrib(array, trgt_pool, src_pool)
+
+            res_str = res_list_to_str(results.values())
+
+            if(i>0):
+                self.assertEqual(res_str, res_str_pre, "Hashes of same sets are not equal")
+
+            res_str_pre = res_str
+
+    def test_equal_source_distribution(self):
+        src_pool, trgt_pool, hsh_fr_trgt = nic.get_lists_for_nodes(self.src[0], self.src[1:], self.targets, len(self.targets))
+        result_cont = {t: 0 for t in src_pool}
+
+        for a in range(5000):
+            str_ = random_string(random.randint(10,100))
+            res_list = nic.calculate_allocs(str_, trgt_pool, src_pool, hsh_fr_trgt)
+                        
+            for l in res_list.values():
+                for p in l:
+                    result_cont[p[0]] += 1
+
+        default = 0
+        for index, value in enumerate(result_cont.values()):
+
+            if index > 0:
+                self.assertEqual(default, value, "Number of targets are not equal for all sources")
+            
+            default = value
