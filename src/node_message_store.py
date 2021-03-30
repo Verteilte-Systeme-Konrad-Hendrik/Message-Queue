e42e20abf1345bffec9d9f491e36aad73224e951
konradhartwig <konrad.hartwig@online.de>
2021-03-30 02:55:31 -0700

    Add unittest for node_message_store.py

--- src/node_message_store.py
+++ src/node_message_store.py
@@ -3,12 +3,14 @@ import node_info
 import json
 import sqlite3
 import os
+import uuid
 
 # contains rounds that have not been completed yet
 pending_rounds = {}
 
 conn = None
 cursor = None
+
 
 def setup_db():
     cursor.execute("""
@@ -23,39 +25,59 @@ def setup_db():
 def init_db(db_name="test.db"):
     global conn, cursor
     conn = sqlite3.connect(db_name)
-    print("Opened database successfully")
+    print("Opened database {} successfully".format(db_name))
     cursor = conn.cursor()
     setup_db()
 
+
 init_db("message.db")
+
 
 def remove_db(db_name="test.db"):
     conn.close()
     os.remove(db_name)
+    print("Closed and removed database {} successfully".format(db_name))
+
 
 def store_messages(messages: []):
     msg_objects = node_message.message_bulk_to_message_array(messages)
 
-    messages = [(msg.__hash__, msg.sender, msg.seq_number, msg.message_content, False) for msg in msg_objects]
+    msg_entries = [(msg.__hash__(), msg.sender.hex, msg.seq_number, msg.message_content, False) for msg in msg_objects]
+
+    # print(msg_entries)
 
     cursor.executemany("""
-        INSERT INTO message VALUES (?,?,?,?,?)
-    """, messages)
+        INSERT INTO message VALUES (?,?,?,?,?);
+    """, msg_entries)
+
+
+def store_messages_in_pool(messages: [], pool):
+    msg_objects = node_message.message_bulk_to_message_array(messages)
+    
+    msg_entries = [(msg.__hash__(), pool.hex) for msg in msg_objects]
+
+    # print(msg_entries)
+
+    cursor.executemany("""
+        INSERT INTO message_pool VALUES (?,?);
+    """, msg_entries)
 
 
 def get_message_for_pool(msg_pool):
-    msg_pool = (msg_pool,)
+    pool_entry = (msg_pool.hex,)
 
     cursor.execute("""
         SELECT sender, seq_number, message FROM message JOIN message_pool ON message.msg_hash = message_pool.msg_hash WHERE message_pool.msg_pool = ?;
-    """, msg_pool)
+    """, pool_entry)
 
-    return cursor.fetchall()
+    msg_entries = cursor.fetchall()
+    messages = [node_message.make_queue_message(uuid.UUID(hex=msg_entry[0]), msg_entry[1], msg_entry[2]) for msg_entry in msg_entries]
+    return messages
 
 
-def store_round(seq):
-    seq = (seq,)
+# def store_round(seq):
+#     seq = (seq,)
 
-    cursor.execute("""
-        UPDATE message SET confirmed = 1 WHERE seq_number = ?;
-    """, seq)
+#     cursor.execute("""
+#         UPDATE message SET confirmed = 1 WHERE seq_number = ?;
+#     """, seq)
--- src/node_misc.py
+++ src/node_misc.py
@@ -1,14 +1,15 @@
 import orchestration_pb2 as orch_pb
 import node_info
+import node_message
 
-def make_example_message(sender, seq_number, content):
-    return orch_pb.QueueMessage(sender=orch_pb.NodeId(nodeId=sender.hex), sequence_number=seq_number,
-                                message_content=content)
+# def make_example_message(sender, seq_number, content):
+#     return orch_pb.QueueMessage(sender=orch_pb.NodeId(nodeId=sender.hex), sequence_number=seq_number,
+#                                 message_content=content)
 
 def get_example_messages():
 
     my_example_messages = {
-        1: [make_example_message(node_info.getNodeInfo().uuid, 1, ("This is a test {}".format(node_info.getNodeInfo().uuid.hex)).encode("UTF-8"))]
+        1: [node_message.make_queue_message(node_info.getNodeInfo().uuid, 1, ("This is a test {}".format(node_info.getNodeInfo().uuid.hex)).encode("UTF-8"))]
     }
 
     return my_example_messages
