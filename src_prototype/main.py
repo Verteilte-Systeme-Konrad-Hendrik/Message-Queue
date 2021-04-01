a1ca2341bd1072ad44d189e057bcb489529da891
konradhartwig <konrad.hartwig@online.de>
2021-04-01 08:17:30 -0700

    Clear up

--- src_prototype/main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("This is only the beginning")
-
-
-if __name__ == "__main__":
-    main()
--- src_prototype/proto_orchestrator_network.py
+++
@@ -1,8 +0,0 @@
-def genereate_node_list_text(node_list):
-    result = ""
-
-    for node in node_list:
-        result += "["+node.uuid.hex+","+node.ip_address+","+str(node.port)+"]"
-
-    return result
-
